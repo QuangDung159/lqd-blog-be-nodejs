505a53676b6c1a2fbb95426c704a85d5d2419a0b
QDLu <quangdunglu159@gmail.com>
2022-12-09 15:00:41 +0700

    Moving code

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-// config dotenv
-require('dotenv').config();
-
+const express = require("express");
 const { connectDB } = require('./configs/db');
-const express = require('express');
 const cors = require('cors');
-const errorHandler = require('./middlewares/errorHandler');
-const authRoute = require('./routes/authRoute');
-const postRoute = require('./routes/postRoute');
-const photoRoute = require('./routes/photoRoute');
-const albumRoute = require('./routes/albumRoute');
 
-connectDB();
+// connectDB();
 const app = express();
 
 // cors
@@ -19,24 +11,13 @@ app.use(cors());
 // body parser
 app.use(express.json());
 
-// mount the route
-app.use('/api/v1/auth', authRoute);
-app.use('/api/v1/post', postRoute);
-app.use('/api/v1/photo', photoRoute);
-app.use('/api/v1/album', albumRoute);
-
-// unhandled routes
-app.all('*', (req, res, next) => {
-    const err = new Error('The route not found');
-    err.statusCode = 404;
-
-    // will trigger errorHandle function with param = err
-    next(err);
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
 });
 
-app.use(errorHandler);
-
-const port = process.env.APP_PORT;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
+app.listen(5001, () => {
+  console.log("Running on port 5001.");
 });
+
+// Export the Express API
+module.exports = app;
